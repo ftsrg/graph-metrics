e1d83270129a2bb98cd52da52f9a0b646809a0a4
Lehel <Lehel44@hotmail.com>
2017-04-01 23:01:38 +0200

    Tried test method in GiraphHelloWorld.java

--- graph-metrics-giraph/src/main/java/GiraphHelloWorld.java
+++ graph-metrics-giraph/src/main/java/GiraphHelloWorld.java
@@ -1,32 +1,40 @@
+import org.apache.giraph.conf.GiraphConfiguration;
 import org.apache.giraph.edge.Edge;
-import org.apache.giraph.GiraphRunner;
 import org.apache.giraph.graph.BasicComputation;
 import org.apache.giraph.graph.Vertex;
+import org.apache.giraph.io.formats.AdjacencyListTextVertexOutputFormat;
+import org.apache.giraph.io.formats.TextDoubleDoubleAdjacencyListVertexInputFormat;
+import org.apache.giraph.utils.InternalVertexRunner;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.NullWritable;
-import org.apache.hadoop.util.ToolRunner;
 
+public class GiraphHelloWorld extends BasicComputation<IntWritable, IntWritable, NullWritable, NullWritable> {
+	final static String[] graphSeed = new String[] { "seed\t0" };
+	
+	@Override
+	public void compute(Vertex<IntWritable, IntWritable, NullWritable> vertex, Iterable<NullWritable> messages) {
+		System.out.print("Hello world from the: " + vertex.getId().toString() + " who is following:");
 
-public class GiraphHelloWorld extends            // Giraph applications are custom classes that
-   BasicComputation<IntWritable, IntWritable,    // typically use BasicComputation class for all its defaults…
-                    NullWritable, NullWritable> {
-  @Override
-  public void compute(Vertex<IntWritable,        // …except for the compute method that has to be defined
-                      IntWritable, NullWritable> vertex,
-                  Iterable<NullWritable> messages) {
-    System.out.print("Hello world from the: " +
-      vertex.getId().toString() + " who is following:");  // priting a “Hello World” message for the current vertex
- 
-    for (Edge<IntWritable, NullWritable> e : vertex.getEdges()) {  // iterating over vertex’s neighbors
-      System.out.print(" " + e.getTargetVertexId());
-    }  
-    System.out.println("");
- 
-    vertex.voteToHalt();  // signaling the end of the current BSP computation for the current vertex 
-  }  
- 
-  public static void main(String[] args) throws Exception {   // main() method is not strictly speaking required,
-    System.exit(ToolRunner.run(new GiraphRunner(), args));    // but it could be useful when using Hadoop’s
-  }                                                           // executor script instead of Giraph’s
+		for (Edge<IntWritable, NullWritable> e : vertex.getEdges()) {
+			System.out.print(" " + e.getTargetVertexId());
+		}
+		System.out.println("");
+
+		vertex.voteToHalt(); // signaling the end of the current BSP computation for the current vertex
+	}
+
+	public void run() {
+		GiraphConfiguration conf = new GiraphConfiguration(); // Giraph configuration
+		conf.setComputationClass(GenerateTwitterParallel.class);
+		conf.setVertexInputFormatClass(TextDoubleDoubleAdjacencyListVertexInputFormat.class);
+		conf.setVertexOutputFormatClass(AdjacencyListTextVertexOutputFormat.class);
+		try {
+			Iterable<String> results = InternalVertexRunner.run(conf, graphSeed);
+			for (String s : results) {
+				System.out.println(s);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 }
-
--- graph-metrics-giraph/src/test/java/TestGiraphApp.java
+++ graph-metrics-giraph/src/test/java/TestGiraphApp.java
@@ -6,42 +6,39 @@ import org.apache.giraph.io.formats.TextDoubleDoubleAdjacencyListVertexInputForm
 import org.apache.giraph.io.formats.AdjacencyListTextVertexOutputFormat;
 import org.apache.giraph.utils.InternalVertexRunner;
 
-public class TestGiraphApp  {
-  final static String[] graphSeed = new String[] { "seed\t0" };
-  final static int EXPECTED_VERTICES = 7; // The number of vertices we expect to
-                                          // be created in the resulting graph
+public class TestGiraphApp {
+	final static String[] graphSeed = new String[] { "seed\t0" };
+	final static int EXPECTED_VERTICES = 7; // The number of vertices we expect to
+											// be created in the resulting graph
 
-  @Ignore("The code this test validates needs to be fixed")
-  @Test
-  public void testNumberOfVertices() throws Exception {
-    GiraphConfiguration conf = new GiraphConfiguration(); // Giraph configuration
-       // object: it will configure our test Giraph run just like a combination of 
-       // static configuration files and command line options we used in all of our 
-       // previous examples
-    conf.setComputationClass(GenerateTwitterParallel.class);
-    conf.setVertexInputFormatClass(
-      TextDoubleDoubleAdjacencyListVertexInputFormat.class);
-    conf.setVertexOutputFormatClass(
-      AdjacencyListTextVertexOutputFormat.class);
+	// @Ignore("The code this test validates needs to be fixed")
+	@Test
+	public void testNumberOfVertices() throws Exception {
+		GiraphConfiguration conf = new GiraphConfiguration(); // Giraph configuration
+		// object: it will configure our test Giraph run just like a combination of
+		// static configuration files and command line options we used in all of our
+		// previous examples
+		conf.setComputationClass(GenerateTwitterParallel.class);
+		conf.setVertexInputFormatClass(TextDoubleDoubleAdjacencyListVertexInputFormat.class);
+		conf.setVertexOutputFormatClass(AdjacencyListTextVertexOutputFormat.class);
 
-    // The following is all that is required for our test to simulate
-    // a full Giraph application run: the conf is how we configure the
-    // simulation and graphSeed is an array of string simulating input
-    // graph data. The result it returns back allows us to iterate over
-    // the simulated output in order to check assumptions about what
-    // we expect after the GenerateTwitterParallel code is done executing.
-    Iterable<String> results =
-          InternalVertexRunner.run(conf, graphSeed);
+		// The following is all that is required for our test to simulate
+		// a full Giraph application run: the conf is how we configure the
+		// simulation and graphSeed is an array of string simulating input
+		// graph data. The result it returns back allows us to iterate over
+		// the simulated output in order to check assumptions about what
+		// we expect after the GenerateTwitterParallel code is done executing.
+		Iterable<String> results = InternalVertexRunner.run(conf, graphSeed);
 
-    // Iterating over the simulated output is how you check the
-    // resulting graph structure. In our case we're simply counting
-    // the number of lines in the simulated output.
-    int totalVertices = 0;
-    for (String s: results ) {
-      totalVertices++;
-    }
-    // We expect a certain number of lines (vertex descriptions)
-    // to be presented in the simulated output.
-    Assert.assertEquals(EXPECTED_VERTICES, totalVertices);
-  }
+		// Iterating over the simulated output is how you check the
+		// resulting graph structure. In our case we're simply counting
+		// the number of lines in the simulated output.
+		int totalVertices = 0;
+		for (String s : results) {
+			totalVertices++;
+		}
+		// We expect a certain number of lines (vertex descriptions)
+		// to be presented in the simulated output.
+		Assert.assertEquals(EXPECTED_VERTICES, totalVertices);
+	}
 }
